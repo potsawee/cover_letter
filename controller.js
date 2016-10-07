7349ebf411dd4d35912c07d271d29a8c2dc83cd2
Edward Liu <yl573@cam.ac.uk>
2016-10-07 23:26:53 +0100

    Added code so that everything works now, but its really not intuitive...

---
+++ controller.js
@@ -0,0 +1,27 @@
+
+
+var app = angular.module('coverLetterApp', [])
+.controller('stringController', ['$scope', function($scope) {
+
+	$scope.letter = '';
+	$scope.company = '';
+	$scope.address = '';
+	$scope.hiring_person = '';
+	$scope.postcode = '';
+	$scope.date = new Date().toDateString();
+
+	$scope.processLetter = function ()
+	{
+		var str = $scope.letter;
+		str = str.replace(/#COMPANY/g, $scope.company)
+				 .replace(/#HIRING_PERSON/g, $scope.hiring_person)
+				 .replace(/#ADDRESS/g, $scope.address)
+				 .replace(/#DATE/g, $scope.date)
+				 .replace(/#POSTCODE/g, $scope.postcode);
+		console.log(str);
+		$scope.letter = str;
+	}
+
+}]);
+
+
