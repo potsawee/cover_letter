46b10e0f06cd0a42f0e4eb045d63855c3175dce0
Edward Liu <yl573@cam.ac.uk>
2016-10-08 16:40:13 +0100

    added tags to UI

--- controller.js
+++ controller.js
@@ -1,6 +1,7 @@
 var app = angular.module('coverLetterApp', [])
 .controller('stringController', ['$scope', function($scope) {
 
+	initial_letter = '';
 	$scope.letter = '';
 	$scope.company = '';
 	$scope.address = '';
@@ -10,14 +11,16 @@ var app = angular.module('coverLetterApp', [])
 
 	$scope.processLetter = function ()
 	{
-		var str = $scope.letter;
-		str = str.replace(/#COMPANY/g, $scope.company)
-				 .replace(/#HIRING_PERSON/g, $scope.hiring_person)
-				 .replace(/#ADDRESS/g, $scope.address)
-				 .replace(/#DATE/g, $scope.date)
-				 .replace(/#POSTCODE/g, $scope.postcode);
-		console.log(str);
-		$scope.letter = str;
+		if(initial_letter == '')
+		{
+			initial_letter = $scope.letter;
+		}
+
+		$scope.letter = initial_letter.replace(/<COMPANY>/g, $scope.company)
+			.replace(/<HIRING_PERSON>/g, $scope.hiring_person)
+			.replace(/<ADDRESS>/g, $scope.address)
+			.replace(/<DATE>/g, $scope.date)
+			.replace(/<POSTCODE>/g, $scope.postcode);
 	}
 
 }]);
