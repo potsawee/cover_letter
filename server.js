74ab43bc3e686fd875ac3e864fb0ac8c5155c972
Edward Liu <yl573@cam.ac.uk>
2016-10-08 17:44:30 +0100

    added nodejs server

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var morgan = require('morgan');
+const PORT = 8080;
+
+var app = express();
+app.use(morgan('dev'));
+app.use(express.static(__dirname));
+
+app.listen(PORT, function() {
+	console.log("Listening on port: %s", PORT);
+});
